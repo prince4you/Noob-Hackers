1212d14526d755dc5a057fa506b3da40feaf60e4
Call me sunil <155473464+prince4you@users.noreply.github.com>
2024-12-31 13:00:31 +0300

    Update addrepo.sh

--- addrepo.sh
+++ addrepo.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo -e "\e[32mInstalling depends..."
-apt install gnupg curl x11-repo -y > /dev/null 2>&1
+apt install gnupg curl x11-repo -y > /dev/null 2>&1 
 
-echo adding sources Noob-Hackers repo
+echo adding sources Noob-Hackers repo 
 sleep 2
 if [[ ! -d $PREFIX/etc/apt/sources.list.d ]]; then
   mkdir -p $PREFIX/etc/apt/sources.list.d > /dev/null 2>&1
-fi
-echo "deb [trusted=yes arch=all] https://raw.githubusercontent.co>
+fi  
+echo "deb [trusted=yes arch=all] https://raw.githubusercontent.com/prince4you/Noob-Hackers/main exploit main" > $PREFIX/etc/apt/sources.list.d/sunil.list
 
 echo -e "\e[32mAdding keys for Noob-Hackers repo\e[0m"
-curl -sL https://raw.githubusercontent.com/prince4you/Noob-Hacker>
+curl -sL https://raw.githubusercontent.com/prince4you/Noob-Hackers/refs/heads/main/NoobHackers.key | apt-key add
 sleep 2
 
-mv $PREFIX/etc/apt/trusted.gpg $PREFIX/etc/apt/trusted.gpg.d/Suni>
+mv $PREFIX/etc/apt/trusted.gpg $PREFIX/etc/apt/trusted.gpg.d/Sunil.gpg > /dev/null 2>&1
 
-printf "\n
+printf "\n 
 Noob-Hackers Repo installed....
 updating repositories...
 "
